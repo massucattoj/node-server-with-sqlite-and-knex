baf2592e421798ae4c53acaabcf40ad57aca7ae1
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2024-06-10 11:02:19 -0300

    node server with sqlite, knex and zod

---
+++ src/routes/transactions.ts
@@ -0,0 +1,78 @@
+// plugin
+
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+
+// Cookies <-> Formas da gente manter contexto entre requisicoes
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'ammount' })
+      .first()
+
+    return {
+      summary,
+    }
+  })
+
+  app.post('/', async (request, reply) => {
+    // { title, amount, type: credit or debit }
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    // verificar se o body da requisicao bate com o schema que eu espero
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      // response
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+}
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
+
+const app = fastify()
+
+// Definir / acoplar plugins (ps: a ordem importa)
+
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
